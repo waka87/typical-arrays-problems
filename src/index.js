a1d12770e595a8e32c63b7ffa1ca2966eb2d5032
waka87 <a.shaumenova87@gmail.com>
2020-10-03 15:56:27 +0600

    fix: change max min and avg

--- src/index.js
+++ src/index.js
@@ -1,47 +1,48 @@
 
 exports.min = function min (array) {
-  var min;
-  if (array.length === 0) {
-    return 0;
-  } else {
-    for ( var i = 0; i < (array.length - 1); i++) {
-      if (array[i] < array[i+1]) {
+  var min = 0;
+  if (array) {
+    for ( var i = 0; i < array.length; i++) {
+      if ( min > array[i]) {
         min = array[i];
-      } else {
-        min = array[i+1];
-      }
+      } 
     }
     return min;
+  } else {
+  return 0;
   }
   
 }
 
 exports.max = function max (array) {
-  var max;
-  if (array.length === 0) {
-    return 0;
-  } else {
-    for ( var i = 0; i < (array.length - 1); i++) {
-      if (array[i] > array[i+1]) {
+  var max = 0;
+  if (array) {
+    for ( var i = 0; i < array.length; i++) {
+      if (max < array[i]) {
         max = array[i];
-      } else {
-        max = array[i+1];
-      }
+      } 
     }
     return max;
+  } else {
+    return 0;
   }
 }
 
 exports.avg = function avg (array) {
   var avg;
   var sum = 0;
-  if (array.length === 0) {
-    return 0;
+  if (array) {
+    if (array.length === 0) {
+      return 0;
+    } 
+      else {
+        for ( var i = 0; i <= (array.length - 1); i++) {
+          sum = sum + array[i];
+        }
+        avg = sum / (array.length);
+        return avg;
+      }
   } else {
-    for ( var i = 0; i <= (array.length - 1); i++) {
-      sum = sum + array[i];
-    }
-    avg = sum / (array.length);
-    return avg;
+    return 0;
   }
 }
