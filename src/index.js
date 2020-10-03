c6f36b1672ea4321f7f2df195d966a9a248a462e
waka87 <a.shaumenova87@gmail.com>
2020-10-03 14:48:57 +0600

    init: start task arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 
 exports.min = function min (array) {
-  return 0;
+  var min;
+  if (array.length === 0) {
+    return 0;
+  } else {
+    for ( var i = 0; i < (array.length - 1); i++) {
+      if (array[i] < array[i+1]) {
+        min = array[i];
+      } else {
+        min = array[i+1];
+      }
+    }
+    return min;
+  }
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  var max;
+  if (array.length === 0) {
+    return 0;
+  } else {
+    for ( var i = 0; i < (array.length - 1); i++) {
+      if (array[i] > array[i+1]) {
+        max = array[i];
+      } else {
+        max = array[i+1];
+      }
+    }
+    return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  var avg;
+  var sum = 0;
+  if (array.length === 0) {
+    return 0;
+  } else {
+    for ( var i = 0; i <= (array.length - 1); i++) {
+      sum = sum + array[i];
+    }
+    avg = sum / (array.length);
+    return avg;
+  }
 }
